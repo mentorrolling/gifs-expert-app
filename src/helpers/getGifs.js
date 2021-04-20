4004b8bf02ba45f411c3a2d5c0ebec2708b8b533
Mentor Rolling <mentor.rolling@gmail.com>
2021-04-20 13:29:47 -0300

    Agregando axios al helpers

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,11 +1,17 @@
+import axios from "axios";
+
 const apiKey = "yGsEUd2oFkyppcVxZBZ5H2aB7uQLZRIf";
 
 export const getGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
     category
   )}&limit=10&api_key=${apiKey}`;
-  const resp = await fetch(url);
-  const { data } = await resp.json();
+
+  const resp = await axios(url);
+  const { data } = resp.data;
+
+  // const resp = await fetch(url);
+  // const { data } = await resp.json();
 
   const gifs = data.map((img) => {
     return {
